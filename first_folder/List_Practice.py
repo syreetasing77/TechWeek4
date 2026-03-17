c856a2ae0402da3b4d2387c153ed1aeb077f7f83
Syreeta <syreeta.singleton96@gmail.com>
2026-03-17 15:07:44 -0400

    added code

---
+++ first_folder/List_Practice.py
@@ -0,0 +1,19 @@
+#Learning how to make lists on pycharm
+my_list = [1,2,3,4,5,6,7,8,9,10]
+print(my_list)
+for num in my_list:
+    print(num) # This line should be indented (pushed to the right)
+mixed_list = ["rice", "water", "salt", (6)]
+print(mixed_list)
+fruits = ["apple", "cherry", "mango"]
+print(fruits)
+fruits.append("kiwi")
+print(fruits)
+fruits.remove("kiwi")
+print(fruits)
+fruits[0] = "banana"
+print(fruits)
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+
+
